6f28c4a2da277c2336f7b744e2e2f46e837855ed
จักรกฤษณ์ <minorin7894@gmail.com>
2022-05-10 17:28:20 +0700

    nodejs

---
+++ models/Employee.js
@@ -0,0 +1,30 @@
+const mongoose  = require('mongoose')
+const Schema    = mongoose.Schema
+
+const employeeSchma = new Schema({
+    name:{
+        type:   String
+    },
+    designation:    {
+        type:   String
+    },
+    email:  {
+        type:   String
+    },
+    phone:  {
+        type:   String
+    },
+    age:    {
+        type:   Number
+    },
+    avatar:    {
+        type:   String
+    }
+
+},{timestamps:  true})
+
+const Employee  = mongoose.model('Employee', employeeSchma)
+module.exports  = Employee
+
+
+
